ea4c78136a5ff7257b10815966c9197c86165453
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-09 14:27:40 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Uppgift_2._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vilken är din fovaritbok");
+            string x = Console.ReadLine();
+            Console.WriteLine("ditt fovarit bok är "+x);
+            string y = Console.ReadLine();
+
+        }
+    }
+}
